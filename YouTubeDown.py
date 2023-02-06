8fe2302f6f675a34f01344a9ecd7270849c4d4af
GuillaumeLeBerreBIT <guillaume.le.berre@student.howest.be>
2023-02-06 06:38:48 -0500

    Updated YouTube Downloader

--- YouTubeDown.py
+++ YouTubeDown.py
@@ -5,41 +5,6 @@
 # Importint the module
 from pytube import YouTube
 
-'''
-# Where to save the video
-save_path = "/home/guest/Videos"
-
-# Link to the viedo to be downloaded
-link = input("Give a YouTube link: ")
-vid_name = input("Name to save the file: ")
-
-try:
-    #Object creation using YouTube
-    #Which was imported in the beginning
-    yt = YouTube(link)
-
-except:
-    print("Connection Error")   #To handle exception
-
-# Filter all the files with "mp4" extension
-mp4files = yt.filter('mp4')
-
-#Set the filename to save the video
-yt.set_filemane(vid_name)
-
-# Get the video with the extension and
-# Resolution passed in the get() function
-
-d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution)
-try:
-    #Downloading the video
-    d_video.download(save_path)
-except:
-    print("Some Error!")
-
-print("Task completed!")
-'''
-
 def Download(link):
     yt_vid = YouTube(link)
     yt_vid = yt_vid.streams.get_highest_resolution()
